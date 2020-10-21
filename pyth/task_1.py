29d14925d340cdace7fbd9ce14b6721ccdfcb7d8
Dmitry Tirskikh <991877@mail.ru>
2020-10-21 19:29:39 +0700

    task 1 is finished

--- pyth/task_1.py
+++ pyth/task_1.py
@@ -17,3 +17,31 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+
+def div_numbers(num1, num2):
+    """
+    Функция выполняет деление двух чисел
+    :param num1: числитель
+    :param num2: знаменатель
+    :return: результат деления
+    """
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print('Вы что? Пытаетесь делить на 0!')
+
+
+while True:
+    try:
+        num_in1 = float(input('Введите первое число: '))
+        num_in2 = float(input('Введите второе число: '))
+    except ValueError:
+        print("Вы ввели не цифру. Попробуйте заново.")
+        continue
+    else:
+        break
+
+div_result = div_numbers(num_in1, num_in2)
+if div_result:
+    print(div_result)
